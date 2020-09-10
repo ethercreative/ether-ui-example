8206b4a78753856989457098704591f48be0fe56
James Edmonston <james@ethercreative.co.uk>
2020-09-10 13:40:57 +0100

    Init home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,134 @@
 import * as React from 'react';
-import { Text } from 'ether-ui';
+import {
+  Grid,
+  Container,
+  Space,
+  Flex,
+  Heading,
+  Column,
+  List,
+  ListItem,
+  Text,
+} from 'ether-ui';
+import Link from 'next/link';
 
-const Home: React.FC = () => <Text>Home</Text>;
+const Home: React.FC = () => (
+  <Grid gap='24'>
+    <header className='bg-gray-200'>
+      <Container>
+        <Space paddingY='5'>
+          <Flex direction='row' alignX='between' alignY='center' wrap='wrap'>
+            <Heading
+              level='1'
+              size={{
+                default: '2xl',
+                sm: '3xl',
+                md: '4xl',
+              }}
+              weight='bold'
+              leading='tight'
+            >
+              Site Name
+            </Heading>
+
+            <Flex direction='row' spaceX='5'>
+              <Link href='/'>Home</Link>
+              <Link href='/'>About</Link>
+              <Link href='/'>News</Link>
+              <Link href='/'>Contact</Link>
+            </Flex>
+          </Flex>
+        </Space>
+      </Container>
+    </header>
+
+    <Container>
+      <Grid
+        columns='12'
+        gapY={{
+          default: '10',
+          md: '0',
+        }}
+      >
+        <Column
+          span={{
+            default: '12',
+            md: '9',
+          }}
+        >
+          <Heading
+            level='1'
+            size={{
+              default: '2xl',
+              sm: '3xl',
+              md: '4xl',
+            }}
+            weight='bold'
+            leading='tight'
+          >
+            Content Heading
+          </Heading>
+        </Column>
+
+        <Column
+          span={{
+            default: '12',
+            md: '3',
+          }}
+        >
+          <Flex direction='col' spaceY='5'>
+            <Heading
+              level='1'
+              size={{
+                default: 'xl',
+                sm: '2xl',
+                md: '3xl',
+              }}
+              weight='bold'
+              leading='tight'
+            >
+              Sidebar Heading
+            </Heading>
+
+            <Text
+              size={{
+                default: 'sm',
+                md: 'base',
+              }}
+              leading='relaxed'
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+              sapiente rerum magnam corporis molestiae consectetur ea vel quae
+              porro neque. Nostrum illum ab libero, suscipit quod unde impedit
+              explicabo dolore.
+            </Text>
+          </Flex>
+        </Column>
+      </Grid>
+    </Container>
+
+    <footer className='bg-gray-200'>
+      <Space paddingY='10'>
+        <Container>
+          <Grid columns='3'>
+            <Column
+              span={{
+                default: '3',
+                md: '1',
+              }}
+            >
+              <List gap='3'>
+                <ListItem>Home</ListItem>
+                <ListItem>About</ListItem>
+                <ListItem>News</ListItem>
+                <ListItem>Contact</ListItem>
+              </List>
+            </Column>
+          </Grid>
+        </Container>
+      </Space>
+    </footer>
+  </Grid>
+);
 
 export default Home;
